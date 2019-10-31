1db96802331c49641fed912655595af6ef63781b
Michael <mschneider247@gmail.com>
2019-10-31 10:31:44 -0600

    Fetch rocket data and set to state in App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { getRocketData } from './apiCalls';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: undefined
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const rocketData = await getRocketData();
+      this.setState({ data: rocketData});
+    } catch ({message}) {
+      console.log('Something Broke!', message)
+    }
+  }
+
+  render () {
+    console.log(this.state.data)
+    let rockets;
+    if (this.state.data !== undefined) {
+      rockets = this.state.data.map(rocket => {
+        return <p>{rocket.rocket_name}</p>
+      })
+    }
+
+    return (
+      <main>
+        <h1>SPACE FARMER</h1>
+        <p>Rocket Stuff:</p>
+        {rockets}
+      </main>
+    )
+  }
 }
 
 export default App;
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,5 @@
-const apiKey = 'yslOAYd9gX47YKpdmsC57cqMA3Agopvd8kmWvVfa'
-
-export const getNasaData = asyn () => {
-  const url = `https://api.nasa.gov/planetary/apod?${apiKey}`
+export const getRocketData = async () => {
+  const url = `https://api.spacexdata.com/v3/rockets`
   const response = await fetch(url);
   const data = response.json();
   return data;
