d722a7510b79e10857df3db2cff828d25f765219
Michael <mschneider247@gmail.com>
2019-10-31 14:35:25 -0600

    Fill out welcome screen with introduction text

--- src/containers/Welcome/Welcome.js
+++ src/containers/Welcome/Welcome.js
@@ -10,8 +10,13 @@ export default class Welcome extends Component {
 
   render() {
     return (
-      <h3>This is the Welcome Container</h3>
+      <div className="div_welcome">
+        <h5>There is a brand new space station somewhere in space... and it needs a farmer.</h5>
+        <h5>For some reason, everyone agrees you are The Farmer.</h5>
+        <h5>The Farmer is you.</h5>
+        <input type="text" placeholder="Enter Your Name"/>
+        <button>Start Farming!</button>
+      </div>
     )
   }
-  
 }
