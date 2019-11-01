797011b03e78262aba946f1c3ace451360d7f7c0
Michael <mschneider247@gmail.com>
2019-11-01 11:23:38 -0600

    Add background image to ProposalContainer

--- src/containers/ProposalContainer/ProposalContainer.js
+++ src/containers/ProposalContainer/ProposalContainer.js
@@ -5,15 +5,15 @@ export const ProposalContainer = () => {
   return (
     <section>
       <section className="space-background">
-        <article>
+        {/* <article>
           <h5>Proposal 1</h5>
         </article>
         <article>
           <h5>Proposal 2</h5>
-        </article>
+        </article> */}
       </section>
-      <section className="section_create-btn">
-        <button>Create Proposal</button>
+      <section className="section_create">
+        <button className="create_btn">Create Proposal</button>
       </section>
     </section>
   )
