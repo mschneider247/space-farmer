a5d9d1aa3066d9e3b11d3f97778eaec52607ca6e
Michael <mschneider247@gmail.com>
2019-11-01 10:28:02 -0600

    Create ProposalContainer and render Route /proposals

---
+++ src/containers/ProposalContainer/ProposalContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProposalContainer.css';
+
+export const ProposalContainer = () => {
+  return (
+    <h1>This is the ProposalContainer</h1>
+  )
+}
+
+export default ProposalContainer;
