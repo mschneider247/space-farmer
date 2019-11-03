bd67a68da9667a69d1ff835f298aa52ab94a2638
Michael <mschneider247@gmail.com>
2019-11-02 19:38:36 -0600

    Update Supplies state when user selects dirt amount

--- src/containers/Supplies/Supplies.js
+++ src/containers/Supplies/Supplies.js
@@ -22,6 +22,11 @@ export class Supplies extends Component {
     this.setState({tractorWeight: weight});
   }
 
+  handleDirt = (cost, weight) => {
+    this.setState({dirtCost: cost});
+    this.setState({dirtWeight: weight});
+  }
+
   render() {
     return (
       <section>
@@ -43,6 +48,23 @@ export class Supplies extends Component {
             <p>Combine</p>
             <img alt="Combine" />
             <input onClick={() => {this.handleTractor(150000, 20000)}} type="radio" name="tractor" value="combine" />
+          </div>
+        </section>
+        <section className="dirt">
+          <div>
+            <p>1 cubic foot of dirt</p>
+            <img alt="1 cubic foot of Dirt" />
+            <input onClick={() => {this.handleDirt(7, 40)}} type="radio" name="dirt" value="1cuFoot" />
+          </div>
+          <div>
+            <p>1 cubic yard of dirt</p>
+            <img alt="1 cubic yard of dirt" />
+            <input onClick={() => {this.handleDirt(40, 1080)}} type="radio" name="dirt" value="1cuYard" />
+          </div> 
+          <div>
+            <p>100 cubic yards of dirt</p>
+            <img alt="100 cubic yards of dirt" />
+            <input onClick={() => {this.handleDirt(3000, 108000)}} type="radio" name="dirt" value="100cuYards" />
           </div>
         </section>
         <Link to='/rockets'><button >Continue</button></Link>
