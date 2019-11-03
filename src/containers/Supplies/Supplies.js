6e97cb043cb8edeaa8b3f10453dbb389490dfa35
Michael <mschneider247@gmail.com>
2019-11-02 19:19:33 -0600

    Update Supplies state when user selects a tractor

--- src/containers/Supplies/Supplies.js
+++ src/containers/Supplies/Supplies.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Supplies.css';
 
 export class Supplies extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tractorCost: 0,
+      tractorWeight: 0,
+      dirtCost: 0,
+      dirtWeight: 0,
+      hoeCost: 0,
+      hoeWeight: 0,
+      seedCost: 0,
+      seedWeight: 0
+    }
+  }
+
+  handleTractor = (cost, weight) => {
+    this.setState({tractorCost: cost});
+    this.setState({tractorWeight: weight});
+  }
+
   render() {
     return (
-      <>
-        <h1>Supplies</h1>
+      <section>
+        <section>
+          <h4>Pick your supplies!</h4>
+        </section>
+        <section className="tractors">
+          <div>
+            <p>Tonka Truck</p>
+            <img alt="Tonka Truck" />
+            <input onClick={() => {this.handleTractor(2, 20)}} type="radio" name="tractor" value="tonka" />
+          </div>
+          <div>
+            <p>Mule</p>
+            <img alt="Mule" />
+            <input onClick={() => {this.handleTractor(150, 2000)}} type="radio" name="tractor" value="mule" />
+          </div>
+          <div>
+            <p>Combine</p>
+            <img alt="Combine" />
+            <input onClick={() => {this.handleTractor(150000, 20000)}} type="radio" name="tractor" value="combine" />
+          </div>
+        </section>
         <Link to='/rockets'><button >Continue</button></Link>
-      </>
+      </section>
     )
   }
 };
