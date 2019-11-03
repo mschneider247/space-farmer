76fc6090cfe4646ad5f0c6ecfd4c1b46ccffa528
Michael <mschneider247@gmail.com>
2019-11-02 23:03:14 -0600

    Add more images for supplies and information for choices

--- src/containers/Supplies/Supplies.js
+++ src/containers/Supplies/Supplies.js
@@ -10,6 +10,9 @@ import hal9000 from '../../images/hal9000.jpg'
 import dirt from '../../images/dirt.jpg';
 import moredirt from '../../images/moredirt.jpg';
 import mostdirt from '../../images/mostdirt.jpg';
+import carrots from '../../images/carrots.jpg';
+import variety from '../../images/variety.jpg';
+import exotic from '../../images/exotic.jpg';
 
 export class Supplies extends Component {
   constructor() {
@@ -58,70 +61,82 @@ export class Supplies extends Component {
         <section className="supplies">
           <section className="tractors">
             <div className="supply-card">
-              <p>Tonka Truck</p>
+              <p>Tonka Truck, $20</p>
+              <p>2 lbs</p>
               <img src={tonka} alt="Tonka Truck" />
               <input onClick={() => {this.handleTractor(20, 2)}} type="radio" name="tractor" value="tonka" />
             </div>
             <div className="supply-card">
-              <p>Mule</p>
+              <p>Mule, $150</p>
+              <p>200 lbs</p>
               <img src={mule} alt="Mule" />
               <input onClick={() => {this.handleTractor(150, 200)}} type="radio" name="tractor" value="mule" />
             </div>
             <div className="supply-card">
-              <p>Combine</p>
+              <p>Combine, $420,000</p>
+              <p>36,000 lbs</p>
               <img src={combine} alt="Combine" />
               <input onClick={() => {this.handleTractor(420000, 36000)}} type="radio" name="tractor" value="combine" />
             </div>
           </section>
           <section className="dirt">
             <div className="supply-card">
-              <p>1 cubic foot of dirt</p>
+              <p>1 cubic foot of dirt, $7</p>
+              <p>40 lbs</p>
               <img src={dirt} alt="1 cubic foot of Dirt" />
               <input onClick={() => {this.handleDirt(7, 40)}} type="radio" name="dirt" value="1cuFoot" />
             </div>
             <div className="supply-card">
-              <p>1 cubic yard of dirt</p>
+              <p>1 cubic yard of dirt, $40</p>
+              <p>1,080 lbs</p>
               <img src={moredirt} alt="1 cubic yard of dirt" />
               <input onClick={() => {this.handleDirt(40, 1080)}} type="radio" name="dirt" value="1cuYard" />
             </div> 
             <div className="supply-card">
-              <p>100 cubic yards of dirt</p>
+              <p>100 cubic yards dirt, $3000</p>
+              <p>108,000 lbs</p>
               <img src={mostdirt} alt="100 cubic yards of dirt" />
               <input onClick={() => {this.handleDirt(3000, 108000)}} type="radio" name="dirt" value="100cuYards" />
             </div>
           </section>
           <section className="hoes">
             <div className="supply-card">
-              <p>Basic hoe</p>
+              <p>Basic hoe, 6$</p>
+              <p>3 lbs</p>
               <img src={hoe} alt="1 basic hoe" />
-              <input onClick={() => {this.handleHoe(4, 15)}} type="radio" name="hoes" value="basicHoe" />
+              <input onClick={() => {this.handleHoe(6, 3)}} type="radio" name="hoes" value="basicHoe" />
             </div>
             <div className="supply-card">
-              <p>Garden tools</p>
+              <p>Garden tools, $35</p>
+              <p>20 lbs</p>
               <img src={gardentools} alt="Garden Tools" />
-              <input onClick={() => {this.handleHoe(10, 45)}} type="radio" name="hoes" value="gardenTools" />
+              <input onClick={() => {this.handleHoe(35, 20)}} type="radio" name="hoes" value="gardenTools" />
             </div> 
             <div className="supply-card">
-              <p>Hal 9000</p>
+              <p>Hal 9000, $97,000</p>
+              <p>16 lbs</p>
               <img src={hal9000} alt="Hal 9000" />
-              <input onClick={() => {this.handleHoe(100000, 12)}} type="radio" name="hoes" value="Hal 9000" />
+              <input onClick={() => {this.handleHoe(97000, 16)}} type="radio" name="hoes" value="Hal 9000" />
             </div>
           </section>
           <section className="seeds">
             <div className="supply-card">
-              <p>Carrot seeds</p>
-              <img alt="carrots" />
+              <p>Carrots, $1</p>
+              <p>0.5 lbs</p>
+              <img src={carrots} alt="carrots" />
               <input onClick={() => {this.handleSeed(1, 0.5)}} type="radio" name="seeds" value="carrots" />
             </div>
             <div className="supply-card">
-              <p>Fruit and Veggie variety</p>
-              <img alt="Fruits and Veggies Pack" />
-              <input onClick={() => {this.handleSeed(10, 1)}} type="radio" name="seeds" value="fruitAndVeggieVariety" />
+              <p>Fruit and Veggie variety, $10</p>
+              <p>4 lbs</p>
+              <img src={variety} alt="Fruits and Veggies Pack" />
+              <input onClick={() => {this.handleSeed(10, 4)}} type="radio" name="seeds" value="fruitAndVeggieVariety" />
             </div> 
             <div className="supply-card">
-              <p>Exotic seeds</p>
-              <img alt="Exotic Seeds" />
-              <input onClick={() => {this.handleSeed(100, 2)}} type="radio" name="seeds" value="exoticSeeds" />
+              <p>Exotics, $110</p>
+              <p>9 lbs</p>
+              <img src={exotic} alt="Exotic Seeds" />
+              <input onClick={() => {this.handleSeed(110, 9)}} type="radio" name="seeds" value="exoticSeeds" />
             </div>
           </section>
         </section>
@@ -131,7 +146,7 @@ export class Supplies extends Component {
               <p id="totalweight">Total Weight: {totalWeight} <span className="symbols">lbs</span></p>
             </div>
             <div className="footer-btn">
-              <Link to='/rockets'><button className="supply-btn">Continue</button></Link>
+              <Link to='/rockets'><button className="supply-btn">Continue to Rockets!</button></Link>
             </div>
           </div>
       </section>
